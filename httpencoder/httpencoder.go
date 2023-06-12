6b93a21dd9c65efd48c6ea39e11bf9b54c864d3e
Tjark Meyer <hello@tjarkmeyer.com>
2023-06-12 13:00:59 +0200

    refactor: http encoder

--- httpencoder/httpencoder.go
+++ httpencoder/httpencoder.go
@@ -7,23 +7,36 @@ import (
 	"time"
 )
 
-func EncodeJson(i interface{}, w http.ResponseWriter, status int) {
+type IHttpEncoder interface {
+	EncodeJson(interface{}, http.ResponseWriter, int)
+	EncodeSuccesful(http.ResponseWriter, int)
+	EncodeFailed(http.ResponseWriter, int)
+	EncodeFileResponse(http.ResponseWriter, *http.Request, string, []byte)
+}
+
+type HttpEncoder struct{}
+
+func NewHttpEncoder() *HttpEncoder {
+	return &HttpEncoder{}
+}
+
+func (h *HttpEncoder) EncodeJson(i interface{}, w http.ResponseWriter, status int) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(status)
 	_ = json.NewEncoder(w).Encode(i)
 }
 
-func EncodeSuccesful(w http.ResponseWriter, status int) {
+func (h *HttpEncoder) EncodeSuccesful(w http.ResponseWriter, status int) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(status)
 	_ = json.NewEncoder(w).Encode("succesful operation")
 }
 
-func EncodeFailed(w http.ResponseWriter, status int) {
+func (h *HttpEncoder) EncodeFailed(w http.ResponseWriter, status int) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(status)
 }
 
-func EncodeFileResponse(w http.ResponseWriter, r *http.Request, filename string, data []byte) {
+func (h *HttpEncoder) EncodeFileResponse(w http.ResponseWriter, r *http.Request, filename string, data []byte) {
 	http.ServeContent(w, r, filename, time.Now(), bytes.NewReader(data))
 }
